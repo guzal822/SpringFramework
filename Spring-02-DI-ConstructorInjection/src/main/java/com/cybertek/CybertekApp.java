d60ddda804289a1d80b2a05feb82f685d500ed71
Guzalinuer Abuduain <Guzalinuer.Abuduain@nttdata.com>
2020-10-23 21:06:37 +0200

    constructor injection

--- Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/CybertekApp.java
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/CybertekApp.java
@@ -1,18 +1,18 @@
-import interfaces.Mentor;
+package com.cybertek;
+
+import com.cybertek.interfaces.Course;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class CybertekApp {
-
     public static void main(String[] args) {
-        // BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
 
         ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
 
-        // Mentor mentor = (Mentor)container.getBean("partTimeMentor");
+        Course course = container.getBean("java", Course.class);
 
-        Mentor mentor = container.getBean("fullTimeMentor",Mentor.class);
+        course.getTeachingHours();
 
-        mentor.createAccount();
+
     }
 }
