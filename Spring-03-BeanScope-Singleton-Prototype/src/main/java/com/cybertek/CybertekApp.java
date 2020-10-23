ccfd9006ac40957758e6750357b166c6602024a2
Guzalinuer Abuduain <Guzalinuer.Abuduain@nttdata.com>
2020-10-23 22:40:10 +0200

    singleton & prototype

--- Spring-03-BeanScope-Singleton-Prototype/src/main/java/com/cybertek/CybertekApp.java
+++ Spring-03-BeanScope-Singleton-Prototype/src/main/java/com/cybertek/CybertekApp.java
@@ -9,10 +9,30 @@ public class CybertekApp {
 
         ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
 
-        Course course = container.getBean("java", Course.class); //container create java object then give it to me
+        // scope="singleton"
+        Course course1 = container.getBean("java", Course.class); //container create java object then give it to me
 
-        course.getTeachingHours();
+        Course course2 = container.getBean("java", Course.class);
 
+        //compare result
+        System.out.println("pointing to the same object ?: " + (course1 == course2));
+        System.out.println("memory location of course1: " + course1);
+        System.out.println("memory location of course2: " + course2);
 
+        //result: course1 and course2 are using the same bean-----singleton
+
+ /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+        // scope="prototype"
+        Course course3 = container.getBean("selenium", Course.class); //container create java object then give it to me
+
+        Course course4 = container.getBean("selenium", Course.class);
+
+        //compare result
+        System.out.println("pointing to the same object ?: " + (course3 == course4));
+        System.out.println("memory location of course3: " + course3);
+        System.out.println("memory location of course4: " + course4);
+
+        //result: course1 and course2 not using the same bean-----prototype
     }
 }
