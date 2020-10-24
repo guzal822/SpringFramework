c0c50acabb2d72c58f50ef62b69ca802910af904
Guzalinuer Abuduain <Guzalinuer.Abuduain@nttdata.com>
2020-10-24 21:00:13 +0200

    configuration

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
@@ -1,14 +1,40 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 public class Java implements Course {
 
-    public void getTeachingHours() {
-        System.out.println("Weekly Teaching Hours : 30");
+    private ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
     }
 
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours : "+(30+extraSessions.getHours()));
+    }
+
+    @Value("JD1")
+    private String batch;
+
+    @Value("${instructor}")
+    private String instructor;
+
+    @Value("${days}")
+    private String[] days;
+
+    @Override
+    public String toString() {
+        return "Java{" +
+                "batch='" + batch + '\'' +
+                ", instructor='" + instructor + '\'' +
+                ", days=" + Arrays.toString(days) +'}';
+    }
 
 }
